3c6527cd14db21850a6c987a4182c560278526da
Patsund <patrik2205@gmail.com>
2017-02-07 16:14:46 +0100

    Add files via upload

--- kvadratur.py
+++ kvadratur.py
@@ -1,65 +1,84 @@
 __author__ = 'Patrik'
 # -*- coding: utf-8 -*-
 #integrators
-def trapezoidIntegral(function, startValue, endValue):
+def trapezoidIntegral(f, startValue, endValue):
     h=endValue-startValue
-    y0=function(startValue)
-    y1=function(endValue)
+    y0=f(startValue)
+    y1=f(endValue)
     return (h/2)*(y0+y1)
-def simpsonIntegral(function, startValue,endValue):
+def simpsonIntegral(f, startValue,endValue): #funker ikke
     x1=startValue+(endValue-startValue)/2
     h=endValue-x1
-    y0=function(startValue)
-    y1=function(x1)
-    y2=function(endValue)
+    y0=f(startValue)
+    y1=f(x1)
+    y2=f(endValue)
     return (h/3)*(y0+4*y1+y2)
 #dummies
 def foo(x):
     return x**4
-"""def betterTrapezoid(function, startValue, endValue, steps):
+"""def betterTrapezoid(f, startValue, endValue, steps):
     stepLength=(endValue-startValue)/steps
     current=startValue
     integral=0
     for i in range(steps):
         #print(current,current+stepLength,"\t")
-        integral+=trapezoidIntegral(function,current,current+stepLength)
+        integral+=trapezoidIntegral(f,current,current+stepLength)
         current+=stepLength
     return integral
-def betterSimpson(function, startValue, endValue, steps):
+def betterSimpson(f, startValue, endValue, steps):
     stepLength=(endValue-startValue)/steps
     current=startValue
     integral=0
     for i in range(steps):
-        integral+=simpsonIntegral(function,current,current+stepLength)
+        integral+=simpsonIntegral(f,current,current+stepLength)
         current+=stepLength
     return integral"""
 #utilities
-def integralWithPartition(integrator,function,startValue,endValue,steps):
+def integralWithPartition(integrator,f,startValue,endValue,steps):
     stepLength=(endValue-startValue)/steps
     current=startValue
     integral=0
     for i in range(steps):
-        integral+=integrator(function,current,current+stepLength)
+        integral+=integrator(f,current,current+stepLength)
         current+=stepLength
     return integral
-def compositeSimpsonFromArray(function,array):
-    h=array[1]-array[0]
-    integral=(h/3)*(function(array[0])+function(array[-1]))
-    m=len(array)/2
+def compositeSimpsonFromArray(f,arr):
+    h=arr[1]-arr[0]
+    integral=(h/3)*(f(arr[0])+f(arr[-1]))
+    m=len(arr)//2
     for i in range(1,m+1):
-        integral+=(4*h/3)*function(array[2*i-1])
+        integral+=(4*h/3)*f(arr[2*i-1])
     for i in range(1,m):
-        integral+=(2*h/3)*function(array[2*i])
+        integral+=(2*h/3)*f(arr[2*i])
     return integral
-
-def compositeTrapezoidFromArray(function,array):
-    integral=function(array[0])*(function(array[1])-function(array[0]))/2+function(array[-1])*(function(array[-11])-function(array[-2]))/2
-    for i in range(1,len(array)-1):
-        integral+=function(array[i])*(function(array[i+1])-function(array[i]))
+"""
+def compositeTrapezoidFromArray(f,arr):
+    integral=f(arr[0])*(f(arr[1])-f(arr[0]))/2+f(arr[-1])*(f(arr[-1])-f(arr[-2]))/2
+    for i in range(1,len(arr)-1):
+        print(i,integral)
+        integral+=f(arr[i])*(f(arr[i+1])-f(arr[i]))
     return integral
-#main function
-def u_avg(function,startValue,endValue,steps):
-    return integralWithPartition(simpsonIntegral,function,startValue,endValue,steps)/(endValue-startValue)
+def betterCompositeTrapezoidFromArray(f,arr):
+    integral=0
+    for i in range(len(arr)-1):
+        integral+=trapezoidIntegral(f,arr[i],arr[i+1])
+    return integral
+def betterCompositeSimpsonFromArray(f,arr):
+    integral=0
+    for i in range(len(arr)-1):
+        integral+=simpsonIntegral(f,arr[i],arr[i+1])
+    return integral"""
+def compositeIntegralFromArray(integrator,f,arr):
+    integral=0
+    for i in range(len(arr)-1):
+        integral+=integrator(f,arr[i],arr[i+1])
+    return integral
+#main f
+def u_avg(f,startValue,endValue,steps):
+    return integralWithPartition(simpsonIntegral,f,startValue,endValue,steps)/(endValue-startValue)
 #test
-print(integralWithPartition(simpsonIntegral,foo,0,3,300000))
-print(integralWithPartition(trapezoidIntegral,foo,0,3,10000))
+#print(integralWithPartition(simpsonIntegral,foo,0,3,300000))
+#print(integralWithPartition(trapezoidIntegral,foo,0,3,10000))
+x=[0,1,2,3]
+print(compositeIntegralFromArray(simpsonIntegral,foo,x))
+print(integralWithPartition(simpsonIntegral,foo,0,3,3))
